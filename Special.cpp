c3cff15d53398702f235c6ffc0808edbd19c8c5f
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-04-25 18:56:14 +0530

    Construtor and Destructor

---
+++ Special.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+        int i;              //characteristics
+        float f;            //characteristics
+
+        Demo()              //Constructor
+        {
+            cout<<"Inside constructor";
+            i=0;
+            f=0.0f;
+        }
+
+        ~Demo()             //Destructor
+        {
+            cout<<"Inside Destructor\n";
+        }
+
+        void fun()          //Behaviour
+        {
+            cout<<"Inside fun\n";
+        }
+};
+
+int main()
+{
+    cout<<"Inside main\n";
+
+    Demo dobj;               // Object Creation
+
+    cout<<"\n"<<dobj.i<<"\n";
+
+    dobj.fun();
+
+    cout<<"End of main\n";
+
+    return 0;
+}
