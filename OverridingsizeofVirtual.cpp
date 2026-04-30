2feadabffea331e79a6a2f0d533f1078c5b67494
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-05-01 03:53:12 +0530

    Create OverridingsizeofVirtual.cpp

---
+++ OverridingsizeofVirtual.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j;
+
+        void fun()
+        {cout<<"Inside Base fun\n"; }
+
+        virtual void gun()
+        {cout<<"Inside Base gun\n"; }
+
+        virtual void sun()
+        {cout<<"Inside Base sun\n";}
+
+};      // 8 byte
+
+class Derived : public Base
+{
+    public:
+        int x,y;
+
+        void fun()        // Redefinition
+        {cout<<"Inside Derived fun\n" ;}
+
+        void sun()        // Redefinition
+        {cout<<"Inside Derived sun\n" ;}
+
+        virtual void run()        // Definition
+        {cout<<"Inside Derived run\n" ;}
+
+};     // 16 bytes
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";         // 8
+    cout<<sizeof(Derived)<<"\n";      // 16
+
+    return 0;
+}
